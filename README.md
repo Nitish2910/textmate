4e14c4abc79c4700017f923340d3b8921bc9447d
Nitish2910 <nitiskumar.g129@gmail.com>
2023-10-16 18:55:15 +0530

    modified-setup-added middlewares

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/Nitish2910/textmate.git
 git push -u origin main
+
+
 …or push an existing repository from the command line
+
 git remote add origin https://github.com/Nitish2910/textmate.git
 git branch -M main
 git push -u origin main
--- textmate_be/src/index.js
+++ textmate_be/src/index.js
@@ -1,12 +1,10 @@
-import dotenv from "dotenv";
-import app from './app.js';
-
-//dotEnv config
-dotenv.config();
+import app from "./app.js";
 
 //env variables
 //check PORT if not then default 8000
 const PORT = process.env.PORT || 8000;
+
+console.log("OK: "+process.env.NODE_ENV);
 
 //listen to PORT defined
 app.listen(PORT,()=>{
